989eae49523f1a294c7e51e49ac185d8c285d4a4
GajananJoshi <gajananjoshi0601@gmail.com>
2025-01-20 05:44:45 +0530

    modified app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import { useState } from 'react';
 import Header from './Header'; 
 import CoreConcepts from './CoreConcepts.jsx';
 import Examples from './Examples.jsx';
 // import { Component } from 'react';
-import { EXAMPLES } from './data.js';
 
 function App() {
   return (
